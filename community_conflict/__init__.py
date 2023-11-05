0ff9bae7223f65c6a7b710c1736eb2c950ba78b9
Elizabeth Nolte <elnccb@umsystem.edu>
2023-11-05 16:25:34 -0600

    basic info computed

--- community_conflict/__init__.py
+++ community_conflict/__init__.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 import networkx as nx
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import powerlaw
 
 assert sys.version_info >= (3, 8), "This script requires Python 3.8 or higher"
 
@@ -20,8 +23,28 @@ def parse_file(file: Path) -> nx.DiGraph:
 
     return graph
 
+def plot_dist(graph):
+    hist = nx.degree_histogram(graph)   
+    plt.plot(range(0, len(hist)), hist, ".")
+    plt.title("Degree Distribution")
+    plt.xlabel("Degree")
+    plt.ylabel("# of Nodes")
+    plt.loglog()
+    plt.show()
+
+
+def basic_info(graph: nx.DiGraph):
+    print("Global Clustering Coefficient:", nx.average_clustering(graph)) # large for real world networks
+    #print("Path length:", nx.average_shortest_path_length(graph)) # small for real world networks
+    print("Density:", nx.density(graph)) # sparse for real world network
+    hist = nx.degree_histogram(graph)
+    plot_dist(graph)
+
+
 
 def main():
     graph = parse_file(Path(".downloads/soc-redditHyperlinks-title.tsv"))
-    print(graph)
+    print(graph) # prints number of edges and nodes
+    basic_info(graph)
+
 
