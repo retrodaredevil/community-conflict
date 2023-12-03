cdb494f7a77f654d0051a7762be5dfe014735905
Ian Tognoni <imtm5c@umsystem.edu>
2023-12-03 17:20:00 -0600

    Graph

--- community_conflict/visualize_graph.py
+++ community_conflict/visualize_graph.py
@@ -7,12 +7,18 @@ from community_conflict.subgraphs import sample_edges
 from community_conflict.subgraphs import sample_nodes
 import matplotlib.pyplot as plt
 
-def draw_graph(graph: nx.graph, layout: int = 0, color_type: bool = False, node_size: int = 50, edge_width: float = 1):
+def draw_graph(graph: nx.graph, layout: int = 3, color_type: bool = False, node_size: int = 50, edge_width: float = 0.75):
     # positions for all nodes and layouts
     if(layout == 0):
-        pos = nx.spring_layout(graph)
+        pos = nx.kamada_kawai_layout(graph) # looks nice, computationaly expensive, and does work well with bigger networks for some reason
+    elif(layout == 1):
+        pos = nx.random_layout(graph) #random places nodes looks squarish
+    elif(layout == 2):
+        pos = nx.shell_layout(graph) #makes an oval
+    elif(layout == 3):
+        pos = nx.spring_layout(graph) #ovalish but with random nodes thrown around
     else:
-        pos = nx.spiral_layout(graph)
+        pos = nx.spiral_layout(graph) #makes a spiral
 
     if(color_type ==  True):
         count = graph.number_of_nodes()
@@ -27,7 +33,7 @@ def draw_graph(graph: nx.graph, layout: int = 0, color_type: bool = False, node_
     nx.draw_networkx_nodes(graph, pos, node_color=colors, node_size=node_size)
 
     # Draw edges
-    nx.draw_networkx_edges(graph, pos, width=edge_width, edge_color='#505050', arrows=True, connectionstyle='arc3, rad = 0.1')
+    nx.draw_networkx_edges(graph, pos, width=edge_width, edge_color='#505050', arrows=True, connectionstyle='arc3, rad = 0')
 
     # Draw labels
     #nx.draw_networkx_labels(graph, pos, font_size=3, font_color='black')
@@ -38,12 +44,12 @@ def draw_graph(graph: nx.graph, layout: int = 0, color_type: bool = False, node_
 
 def main():
     graph = read_or_parse_file(Path(".downloads/soc-redditHyperlinks-title.tsv"), Path(".cache/soc-redditHyperlinks-title.pickle"))
-    edgeSub = sample_edges(graph, 1000)
-    #print(edgeSub)
+    edgeSub = sample_edges(graph, 500)
+    print(edgeSub)
     #nodeSub = sample_nodes(graph, 50)
     #print(nodeSub)
-    #print_graph(edgeSub)
-    draw_graph(edgeSub, color_type = True, node_size = 50, edge_width= 0.5)
+    draw_graph(edgeSub, layout = 2)
+    #draw_graph(nodeSub, layout = 0)
 
 if __name__ == '__main__':
     main()
